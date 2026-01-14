22b938d873005f3e0215fa6de80e47c9544191cf
Aariz18 <mohd.aariz13518@gmail.com>
2026-01-14 23:34:51 +0530

    Added bootstrap navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@
 import './App.css'
 // import Apicall from './components/Apicall'
 import Everything from './components/Everything'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
-    // <Apicall/>
+    <>
+    <Navbar/>
     <Everything/>
+    </>
   )
 }
 
